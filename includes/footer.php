a5615c343f0153e7f8e93f92be97a53a64c3388a
adi1723 <132340076+adi1723@users.noreply.github.com>
2024-11-26 12:58:50 +0530

    Add files via upload

---
+++ includes/footer.php
@@ -0,0 +1,8 @@
+<footer class="main-footer">
+    <div class="container">
+      <div class="pull-right hidden-xs">
+        <b>Voting System</b>
+      </div>
+      <strong> &copy; 2024 Brought To You By Aditya Kapadane</strong>
+    </div>
+</footer>
